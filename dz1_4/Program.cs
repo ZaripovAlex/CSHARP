8f19e779b1fd962fa2e88ad5c79f343e709489c1
ZaripovAlexey <zaripovalex@mail.ru>
2022-09-16 19:38:43 +0500

    доделал

--- dz1_4/Program.cs
+++ dz1_4/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число ") ;
+int n =Int32.Parse(Console.ReadLine());
+int i=1 ;
+while (i<n)
+{
+  if(i%2==0) Console.WriteLine(i);
+  i++; 
+}
