1433984f1b4ae78785f93d5ff6a92c184391143c
Duke <zaripovalex@mal.ru>
2022-09-17 13:36:59 +0500

    S1_2

--- s1_2/Program.cs
+++ s1_2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num==1) Console.WriteLine("Понедельник");
+if (num==2) Console.WriteLine("Вторник");
+if (num==3) Console.WriteLine("Среда");
+if (num==4) Console.WriteLine("Четверг");
+if (num==5) Console.WriteLine("Пятница");
+if (num==6) Console.WriteLine("Суббота");
+if (num==7) Console.WriteLine("Воскресенье");
+if (num>7) Console.WriteLine("Такогодня недели не бвает");
