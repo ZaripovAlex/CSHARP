633b32c9ddc347de60297dfe84304c9b2347375d
Duke <zaripovalex@mal.ru>
2022-09-17 18:05:45 +0500

    Доделал

--- s1_3/Program.cs
+++ s1_3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N
+
+Console.Write("Введите число ");
+int N = Int32.Parse(Console.ReadLine());
+int i = N* -1;
+if (N<i)
+{
+    i*=-1;
+    N*=-1;
+}
+while(i<N)
+{
+    Console.WriteLine(i);
+    i++;
+}
