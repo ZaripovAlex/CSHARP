24a1528480013672d72d2d16f58b42aeaf0aab20
Duke <zaripovalex@mal.ru>
2022-09-17 13:18:45 +0500

    S1_1

--- s1_1/Program.cs
+++ s1_1/Program.cs
@@ -1,4 +1,13 @@
-﻿Console.Write("Введите число ");
-int num = Convert.ToInt32(Console.ReadLine());
-int result =num*num;
-Console.Write ($"Квадрат числа {num} :{result}"); 
+﻿Console.Write("Введите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int result =num2*num2;
+if (result==num1)
+{
+    Console.Write ($"Число {num2} является квадратом числа {num1}"); 
+}
+else
+{
+    Console.Write ($"Число {num2} неявляется квадратом числа {num1}"); 
+} 
