5e90350c191a21246f3930fc530cca26259a6e1f
Duke <zaripovalex@mal.ru>
2022-09-17 13:02:14 +0500

    S1_1

--- s1_1/Program.cs
+++ s1_1/Program.cs
@@ -1,3 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-11
+﻿Console.Write("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result =num*num;
+Console.Write ($"Квадрат числа {num} :{result}"); 
