a537a414a1405e92df97afbec3064205c0d3bd24
Duke <zaripovalex@mal.ru>
2022-09-17 18:23:21 +0500

    s1_4

--- s1_4/Program.cs
+++ s1_4/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа
+Console.Clear();
+Console.Write("Введите число ");
+int N = Int32.Parse(Console.ReadLine());
+if ((N>99)&(N<1000))
+{
+    Console.WriteLine(N%10);
+} 
+else
+{
+    Console.WriteLine("Неправильное число");
+}
+
