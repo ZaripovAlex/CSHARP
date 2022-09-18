5cb02ebd3ffb14dd0151f9ff00be6d6e76b39bb9
Duke <zaripovalex@mal.ru>
2022-09-18 15:41:52 +0500

    dz2_1

--- dz2_1/Program.cs
+++ dz2_1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+
+Console.Write("Введите число ");
+int num = Int32.Parse(Console.ReadLine());
+if ((num>99) & (num<1000))
+{
+    int num1=num/10;
+    int num2=num1%10;
+    Console.WriteLine(num2);
+}
+else 
+{
+    Console.WriteLine("Неверное число");
+}
