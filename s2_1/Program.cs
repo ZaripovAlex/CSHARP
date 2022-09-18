b771b2956a9d35eb676798771b7956627a21cb0e
Duke <zaripovalex@mal.ru>
2022-09-18 12:45:14 +0500

    s2_1

--- s2_1/Program.cs
+++ s2_1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Ввдите число");
+int num =new Random().Next(10,99);
+int num1= num%10;
+int num2=num/10;
+if (num1>num2)
+{
+    Console.WriteLine(num1);
+}
+else
+{
+    Console.WriteLine(num2);
+}
+Console.WriteLine(num);
