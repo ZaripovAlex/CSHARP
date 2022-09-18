6f56d9b5b3389c3fd053a8e618028eaff19f077d
Duke <zaripovalex@mal.ru>
2022-09-18 15:14:17 +0500

    s2_2

--- s2_2/Program.cs
+++ s2_2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+//456 -> 46
+//782 -> 72
+//918 -> 98
+
+int num = new Random().Next(100, 999);
+Console.WriteLine(num);
+int num1 = num % 10;
+int num2 = num / 100;
+
+Console.WriteLine();
+Console.WriteLine(num1);
+Console.WriteLine(num2);
+Console.WriteLine(num2 * 10 + num1);
