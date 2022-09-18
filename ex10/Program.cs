1ec944772b098cd0e9cd13682c16f4456e846e0e
Duke <zaripovalex@mal.ru>
2022-09-18 11:35:59 +0500

    s2_1

--- ex10/Program.cs
+++ ex10/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Clear;
+int[] array = new int[10];
+void FillArray(int[] col)
+{
+    int n= col.Length;
+    int i=0;
+    while (i<n)
+    {
+        col[i]=new Random().Next(1,10);
+        i++;
+    }
+}
+void PrintArray(int[] arr)
+{
+    int n = arr.Length;
+    int i =0;
+    while (i<n)
+    {
+        Console.WriteLine(arr[i]);
+    }
+}
+int[] IndexOf(int[] arr, int f)
+{
+    int n=arr.Length;
+    int i=0;
+    while(i<n)
+    {
+        if(arr[i]==f)
+        {
+            int pos = i;
+            break;
+        }
+        
