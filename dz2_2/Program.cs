16542fa23b70f870ca7481268a5ab1300930bddb
Duke <zaripovalex@mal.ru>
2022-09-18 15:54:33 +0500

    dz2_2

--- dz2_2/Program.cs
+++ dz2_2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.Write("Введите число ");
+int num = Int32.Parse(Console.ReadLine());
+int num1=0;
+if (num<100) 
+{
+   Console.WriteLine("Третьего числа нет");
+}
+else 
+{
+    num1=num%10;
+    Console.Write($"Третье число - {num1}");
+}
