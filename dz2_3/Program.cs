e7fa877d811e898bc66ab870dba8b270e38deb9c
Duke <zaripovalex@mal.ru>
2022-09-18 16:13:35 +0500

    dz2_3

--- dz2_3/Program.cs
+++ dz2_3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите номер дня недели ");
+int num = Int32.Parse(Console.ReadLine());
+if((num>0) && (num<6))
+{
+     Console.Write("Будни ");
+}
+else if ((num==6)||(num==7)) 
+{
+    Console.Write( "Выходной!!!");
+}
+else
+{
+     Console.Write("Такой хоккей нам не нужен!");
+}
