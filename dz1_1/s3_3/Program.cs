f697e98691942b35d60532d081ebc261e2f38bff
ZaripovAlexey <zaripovalex@mail.ru>
2022-09-24 13:34:08 +0500

    s3_3

---
+++ dz1_1/s3_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.    d = √ (х 1 – х 2) 2 + (у1 – у 2) 2
+
+Console.Clear();
+Console.Write("Введите число ");
+int x1 = Int32.Parse(Console.ReadLine());
+Console.Write("Введите число ");
+int y1 = Int32.Parse(Console.ReadLine());
+Console.Write("Введите число ");
+int x2 = Int32.Parse(Console.ReadLine());
+Console.Write("Введите число ");
+int y2 = Int32.Parse(Console.ReadLine());
+double d=Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2));
+Console.WriteLine(d);
