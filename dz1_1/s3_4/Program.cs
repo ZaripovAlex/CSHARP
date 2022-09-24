60a5d60912ef7e077a1fcd4fb5dad5a6ac691e94
ZaripovAlexey <zaripovalex@mail.ru>
2022-09-24 13:42:32 +0500

    s3_4

---
+++ dz1_1/s3_4/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+Console.Clear();
+Console.Write("Введите число ");
+int n = Int32.Parse(Console.ReadLine());
+for(int i=0; i<n; i++)
+  Console.WriteLine($"{Math.Pow(i,2)} ");  
