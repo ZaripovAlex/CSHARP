5d897329b27796976467aaa05c5e8d1d40bd5af4
ZaripovAlexey <zaripovalex@mail.ru>
2022-09-24 13:20:11 +0500

    s3_2

---
+++ dz1_1/s3_2/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 18: Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+Console.Clear();
+Console.Write("Введите число ");
+int a = Int32.Parse(Console.ReadLine());
+if (a==1) Console.WriteLine("X>0 Y>0");
+if (a==2) Console.WriteLine("X<0 Y>0");
+if (a==3) Console.WriteLine("X<0 Y<0");
+if (a==4) Console.WriteLine("X>0 Y<0");
+
+
