ea4341b35e6c0b4ce6de20ccf3fb8e673f12b2e8
ZaripovAlexey <zaripovalex@mail.ru>
2022-09-25 14:15:14 +0500

    s4_4

---
+++ s4_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов,
+//заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+
+
+Console.Clear();
+int[] arrey = new int[8];
+Console.Write("[");
+for(int i =  0; i < 8; i++)
+{
+arrey[i] =  new Random().Next(0, 2);
+Console.Write($"{arrey[i]}, ");
+}
+Console.Write("]");
