5815855f4b7d059943f426d7edbda90aa6a26aaf
ZaripovAlexey <zaripovalex@mail.ru>
2022-09-25 17:21:59 +0500

    dz4_3

---
+++ dz4_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+int[] arrey = new int[8];
+Console.Write("[");
+for(int i =  0; i < 8; i++)
+{
+arrey[i] =  new Random().Next(0, 100);
+Console.Write($"{arrey[i]}, ");
+}
+Console.Write("]");
