cb0c463c0127f20576a1a1e5ac6473b8cd6dc860
ZaripovAlexey <zaripovalex@mail.ru>
2022-09-25 14:06:31 +0500

    s4_1

---
+++ s4_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.Clear();
+Console.Write("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int S = 0;
+for(int i = 1; i <= N; i++)
+{
+    S = S + i;
+}
+Console.Write($"{N} -> {S}");
