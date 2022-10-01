2f4646897168f8954f889ec3b3df89522fe2ca8b
ZaripovAlexey <zaripovalex@mail.ru>
2022-10-01 17:20:57 +0500

    dz5_2

---
+++ dz5_2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        array[index] = new Random().Next(0, 11);
+        //index = index + 1;
+        index++;
+    }
+}
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{array[position]},");
+        position++;
+    }
+    Console.WriteLine();
+}
+Console.Clear();
+Console.Write("Введите количество элементов мвссива ");
+int n = Convert.ToInt32(Console.ReadLine());
+int rez=0;
+int[] ar = new int [n];
+FillArray(ar);
+PrintArray(ar);
+for(int i=1; i<n; i+=2)
+{
+    rez=rez+ar[i];
+}
+Console.Write($"Сумма элементов, стоящих на нечётных позициях: - {rez}");
