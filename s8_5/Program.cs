28abdf4cfce4273c63caf3eda93cd55d0407d7c8
ZaripovAlexey <zaripovalex@mail.ru>
2022-10-09 17:41:44 +0500

    s8_5

---
+++ s8_5/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 61: Вывести первые N строк треугольника Паскаля. Сделать вывод в виде равнобедренного треугольника
+
+
+float Factorial(int n)
+        {
+            float i, x = 1;
+            for (i = 1; i <= n; i++)
+            {
+                x *= i;
+            }
+            return x;
+        }
+Console.Clear();
+Console.Write("Введите количество строк в таблице. ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < n; i++)
+            {
+                for (int c = 0; c <= (n - i); c++) 
+                {
+                    Console.Write(" "); 
+                }
+                for (int c = 0; c <= i; c++)
+                {
+                    Console.Write(" ");
+                    Console.Write(Factorial(i) / (Factorial(c) * Factorial(i - c)));
+                }
+                Console.WriteLine();
+                
+            }            
