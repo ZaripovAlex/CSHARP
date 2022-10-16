9288f451eda6eadcce2488a95bdc706a4d94e4a8
Duke <zaripovalex@mal.ru>
2022-10-16 20:00:05 +0500

    s9_3

---
+++ s9_3/Program.cs
@@ -0,0 +1,16 @@
+﻿//ПРограмма просит ввести число и выдает сумму чисел
+
+
+Console.Clear();
+int SumNum(int n)
+{
+    int s = 0;
+    if (n / 10 > 0)
+        return s + n % 10 + SumNum(n / 10);
+    else
+        return n % 10;
+}
+Console.WriteLine("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumNum(n));
+
